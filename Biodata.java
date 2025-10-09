06ef06ca60a13577b55f76e31f28a437ad8d1c22
vishnuvardhinij <vishnuvardhini.j@gmail.com>
2025-10-09 19:48:41 +0530

    third commit9/9/25hw

---
+++ Biodata.java
@@ -0,0 +1,21 @@
+class Biodata
+{
+	public static void main(String[] args)
+	{
+		String Name="vishnu vardhini";
+		char initial='j';
+		String FatherName="jeyabalan";
+		char Finitial='s';
+		String motherName="kavitha";
+		char Minitial='j';
+		int date=20;
+		int month=03;
+		int year=2002;
+		String area="madurai";
+		System.out.println("my name is:"+Name+"."+initial);
+		System.out.println("my fathername is:"+FatherName+"."+Finitial);
+		System.out.println("my mothername is:"+motherName+"."+Minitial);
+		System.out.println("my date of birth is:"+date+"/"+month+"/"+year);
+		System.out.println("my location is:"+area);
+	}
+}
