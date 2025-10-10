181837d000c525d9c6fb9868ea0582e03b15d212
vishnuvardhini2028-app <vishnuvardhini2028@gmail.com>
2025-10-10 21:03:04 +0530

    Add files via upload

---
+++ HW 10.10.25/ModulasOperator.java
@@ -0,0 +1,10 @@
+class ModulasOperator
+{
+	public static void main(String[] args)
+	{
+		int a=99;
+		int b=10;
+		int c=a%b;
+		System.out.println("The modulas of two value is:"+c);
+	}
+}
---
+++ HW 10.10.25/MultiplicationOperator.java
@@ -0,0 +1,10 @@
+class MultiplicationOperator
+{
+	public static void main(String[] args)
+	{
+		int a=25;
+		int b=4;
+		int c=a*b;
+		System.out.println("The multiplication of two value is:"+c);
+	}
+}
