7b2c448adf3f52505e91110a813872db86ff7feb
vishnuvardhinij <vishnuvardhini.j@gmail.com>
2025-10-10 20:20:32 +0530

    third commit 10/10/25

---
+++ DivisionOperator.java
@@ -0,0 +1,10 @@
+class DivisionOperator
+{
+	public static void main(String[] args)
+	{
+		int a=125;
+		int b=5;
+		int c=a/b;
+		System.out.println("The division of two value is:"+c);
+	}
+}
---
+++ SubtractionOperator.java
@@ -0,0 +1,10 @@
+class SubtractionOperator
+{
+	public static void main(String[] args)
+	{
+		int a=30;
+		int b=18;
+		int c=a-b;
+		System.out.println("The subtraction of two value is:"+c);
+	}
+}
