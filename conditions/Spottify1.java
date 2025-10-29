1e85653594005ce486bc298ec3365c53c8869539
vishnuvardhini2028-app <vishnuvardhini2028@gmail.com>
2025-10-29 18:05:31 +0530

    Add files via upload

---
+++ conditions/Spottify1.java
@@ -0,0 +1,679 @@
+import java.util.Scanner;
+class Spottify1
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("music directors");
+		System.out.println("1.ILLAIYARAJA ");
+		System.out.println("2.A.R.RAHMAN. ");
+		System.out.println("3.HARISH JEYARAJ ");
+		System.out.println("4.U1");
+		System.out.println("5.ANIRUDH");
+		System.out.println("select your options");
+		System.out.println("1.melody");
+		System.out.println("2.kuthu");
+		System.out.println("3.sad");
+		System.out.println("4.motivational songs");
+		System.out.println("select your options");
+		int song=sc.nextInt();
+		if(song==1)
+		{
+			System.out.println("ILLAIYARAJA SONGS LISTS ");
+			System.out.println("melody songs ");
+			System.out.println("1.Oru kili uruguthu ");
+			System.out.println("2.Kuzhaloodhum kannanukku  ");
+			System.out.println("3.Ennai thottu ");
+			System.out.println("4.Raja Raja cholan  ");
+		    System.out.println("select your options");
+			int choose=sc.nextInt();
+			if(choose==1)
+			{
+			   System.out.println("1.Oru kili uruguthu ");
+				
+			}
+			else if(choose==2)
+			{
+			   System.out.println("2.Kuzhaloodhum kannanukku  ");
+				
+			}
+			else if(choose==3)
+			{
+			   System.out.println("3.Ennai thottu ");
+				
+			}
+			else if(choose==4)
+			{
+			    System.out.println("4.Raja Raja cholan  ");
+				 
+			}
+			else
+			{
+			    System.out.println("sorry songs is not in the list ");
+				
+			}
+		    System.out.println("kuthu songs ");
+			System.out.println("1.Ooruvittu ooruvanthu  ");
+			System.out.println("2.Nooru varusham ");
+			System.out.println("3.Aasai Nooruvagai  ");
+			System.out.println("4.Aasai Athigam vachu  ");
+		    System.out.println("select your options");
+			//int choose=sc.nextInt();
+		    if(choose==1)
+			{
+			   
+			   System.out.println("1.Ooruvittu ooruvanthu  ");
+				
+			}
+			else if(choose==2)
+			{
+			   
+			   System.out.println("2.Nooru varusham ");
+				
+			}
+			else if(choose==3)
+			{
+			    System.out.println("3.Aasai Nooruvagai  ");
+		         
+				
+			}
+			else if(choose==4)
+			{
+			    
+			     System.out.println("4.Aasai Athigam vachu  ");
+				 
+			}
+			else
+			{
+			    System.out.println("sorry songs is not in the list ");
+				
+			}
+			System.out.println("sad songs ");
+			System.out.println("1.Kanne Kalaimane  ");
+			System.out.println("2.Thene thenpaandi Neeye ");
+			System.out.println("3.Paadi Parantha Killi  ");
+			System.out.println("4. Nilave Vaa  ");
+		    System.out.println("select your options");
+			//int choose=sc.nextInt();
+		    if(choose==1)
+			{
+			  System.out.println("1.Kanne Kalaimane  ");
+			   
+			}
+			else if(choose==2)
+			{
+			   
+			  System.out.println("2.Thene thenpaandi Neeye ");
+				
+			}
+			else if(choose==3)
+			{
+			  System.out.println("3.Paadi Parantha Killi  ");
+		      
+				
+			}
+			else if(choose==4)
+			{
+			    System.out.println("4. Nilave Vaa  ");
+			}	
+			else
+			{
+			    System.out.println("sorry songs is not in the list ");
+				
+			}
+		}	
+		else if(song==2)
+        {
+			System.out.println("A.R.RAHMAN SONGS LISTS ");
+			System.out.println("melody songs ");
+			System.out.println("1.Netru illadha Matram  ");
+			System.out.println("2.Enna Solla Pogirai  ");
+			System.out.println("3.Ennavale Adi Ennavale  ");
+			System.out.println("4.Malargale Malargale   ");
+			System.out.println("select the options   ");
+			int pick=sc.nextInt();
+			if(pick==1)
+			{
+			   System.out.println("1.Netru illadha Matram ");
+				
+			}
+			else if(pick==2)
+			{
+			   System.out.println("2.Enna Solla Pogirai  ");
+				
+			}
+			else if(pick==3)
+			{
+			   System.out.println("3.Ennavale Adi Ennavale ");
+				
+			}
+			else if(pick==4)
+			{
+			    System.out.println("4.Malargale Malargale  ");
+				 
+			}
+			else
+			{
+			    System.out.println("sorry songs is not in the list ");
+				
+			}
+		
+			System.out.println("kuthu songs ");
+			System.out.println("1.Thaiya Thaiya   ");
+			System.out.println("2.Mukkala Mukkabala ");
+			System.out.println("3.Urvashi Urvashi   ");
+			System.out.println("4.Aathichudi   ");
+		    System.out.println("select your options");
+			//int pick=sc.nextInt();
+		    if(pick==1)
+			{
+			   
+			   System.out.println("1.Thaiya Thaiya ");
+				
+			}
+			else if(pick==2)
+			{
+			   
+			   System.out.println("2.Mukkala Mukkabala  ");
+				
+			}
+			else if(pick==3)
+			{
+			    System.out.println("3.Urvashi Urvashi  ");
+		         
+				
+			}
+			else if(pick==4)
+			{
+			    
+			     System.out.println("4.Aathichudi ");
+				 
+			}
+			else
+			{
+			    System.out.println("sorry songs is not in the list ");
+				
+			}
+			System.out.println("motivational songs ");
+			System.out.println("1.Vande Mataram    ");
+			System.out.println("2.Oruvan Oruvan mudhalli  ");
+			System.out.println("3.Shakthi kodu    ");
+			System.out.println("4.Kappal Yeari Poyachu    ");
+		    System.out.println("select your options");
+			//int pick=sc.nextInt();
+		    if(pick==1)
+			{
+			   
+			   System.out.println("1.Vande Mataram  ");
+				
+			}
+			else if(pick==2)
+			{
+			   
+			   System.out.println("2.Oruvan Oruvan mudhalli   ");
+				
+			}
+			else if(pick==3)
+			{
+			    System.out.println("3.Shakthi kodu   ");
+		         
+				
+			}
+			else if(pick==4)
+			{
+			    
+			     System.out.println("4.Kappal Yeari Poyachu  ");
+				 
+			}
+			else
+			{
+			    System.out.println("sorry songs is not in the list ");
+				
+			}
+			System.out.println("sad songs ");
+			System.out.println("1.Poongatrile ");
+			System.out.println("2.Marudaani");
+			System.out.println("3.Minnalea En vazhvin");
+			System.out.println("4.Rasathi");
+			System.out.println("5.Oru poiyavathu Sol ");
+		    System.out.println("select your options");
+			//int pick=sc.nextInt();
+		    if(pick==1)
+			{
+			   
+			   System.out.println("1.Poongatrile   ");
+				
+			}
+			else if(pick==2)
+			{
+			   
+			   System.out.println("2.Marudaani   ");
+				
+			}
+			else if(pick==3)
+			{
+			    System.out.println("3.Minnalea En vazhvin    ");
+		         
+				
+			}
+			else if(pick==4)
+			{
+			    
+			     System.out.println("4.Rasathi   ");
+				 
+			}
+			else if(pick==5)
+			{
+				System.out.println("5.Oru poiyavathu Sol ");
+			}
+			else
+			{
+			    System.out.println("sorry songs is not in the list ");
+				
+			}
+		}	
+		else if(song==3)
+		{
+			System.out.println("HAARISH JEYARAJ SONGS LISTS ");
+		    System.out.println(" melody songs ");
+			System.out.println("1.Unnale    ");
+			System.out.println("2.Annul Maele   ");
+			System.out.println("3.Yedho Ondru     ");
+			System.out.println("4.Vaseegara     ");
+			System.out.println("5.Paartha Mudhal  ");
+		    System.out.println("select your options");
+			int slct=sc.nextInt();
+			if(slct==1)
+			{
+			   
+			   System.out.println("1.Unnale   ");
+				
+			}
+			else if(slct==2)
+			{
+			   
+			   System.out.println("2.Annul Maele    ");
+				
+			}
+			else if(slct==3)
+			{
+			    System.out.println("3.Yedho Ondru    ");
+		         
+				
+			}
+			else if(slct==4)
+			{
+			    
+			     System.out.println("4.Vaseegara   ");
+				 
+			}
+			else if(slct==5)
+			{
+				System.out.println("5.Paartha Mudhal  ");
+			}
+			else
+			{
+			    System.out.println("sorry songs is not in the list ");
+				
+			}
+			System.out.println("kuthu songs ");
+			System.out.println("1.Tirunelveli Halwa Da    ");
+			System.out.println("2.Aradi katre  ");
+			System.out.println("3.Soda Bottle    ");
+			System.out.println("4.Aruva Meesai   ");
+			//int slct=sc.nextInt();
+		    if(slct==1)
+			{
+			   
+			   System.out.println("1.Tirunelveli Halwa Da  ");
+				
+			}
+			else if(slct==2)
+			{
+			   
+			   System.out.println("2.Aradi katre   ");
+				
+			}
+			else if(slct==3)
+			{
+			    System.out.println("3.Soda Bottle   ");
+		         
+				
+			}
+			else if(slct==4)
+			{
+			    
+			     System.out.println("4.Aruva Meesai  ");
+				 
+			}
+			else
+			{
+			    System.out.println("sorry songs is not in the list ");
+				
+			}
+			System.out.println("sad songs ");
+			System.out.println("1.Venmathiye  ");
+			System.out.println("2.Oh maname ");
+			System.out.println("3.Yamma Yamma ");
+			System.out.println("4.Othayile");
+		    System.out.println("select your options");
+			//int slct=sc.nextInt();
+		    if(slct==1)
+			{
+			   
+			   System.out.println("1.Venmathiye    ");
+				
+			}
+			else if(slct==2)
+			{
+			   
+			   System.out.println("2.Oh maname   ");
+				
+			}
+			else if(slct==3)
+			{
+			    System.out.println("3.Yamma Yamma     ");
+		         
+				
+			}
+			else if(slct==4)
+			{
+			    
+			     System.out.println("4.Othayile   ");
+				 
+			}
+			else
+			{
+			    System.out.println("sorry songs is not in the list ");
+				
+			}
+		}	
+		else if(song==4)
+		{
+		    System.out.println("U1 SONGS LISTS ");
+			System.out.println("melody songs ");
+			System.out.println("1.Aathadi Manasudhan   ");
+			System.out.println("2.Chinna sirisu  ");
+			System.out.println("3.Venmeggam   ");
+			System.out.println("4.Iayyayyo   ");
+		    System.out.println("select your options");
+			int check=sc.nextInt();
+			if(check==1)
+			{
+			   System.out.println("1.Aathadi Manasudhan  ");
+				
+			}
+			else if(check==2)
+			{
+			   System.out.println("2.Chinna sirisu  ");
+				
+			}
+			else if(check==3)
+			{
+			   System.out.println("3.Venmeggam  ");
+				
+			}
+			else if(check==4)
+			{
+			    System.out.println("4.Iayyayyo  ");
+				 
+			}
+			else
+			{
+			    System.out.println("sorry songs is not in the list ");
+				
+			}
+			System.out.println("kuthu songs ");
+			System.out.println("1.Machi open the bottle    ");
+			System.out.println("2.Saroja saman nikalo   ");
+			System.out.println("3.No money no honey   ");
+			System.out.println("4.Villaiyadu mankatha   ");
+		    System.out.println("select your options");
+			//int check=sc.nextInt();
+		    if(check==1)
+			{
+			   System.out.println("1.Machi open the bottle   ");
+				
+			}
+			else if(check==2)
+			{
+			   System.out.println("2.Saroja saman nikalo   ");
+				
+			}
+			else if(check==3)
+			{
+			   System.out.println("3.No money no honey  ");
+				
+			}
+			else if(check==4)
+			{
+			    System.out.println("4.Villaiyadu mankatha   ");
+				 
+			}
+			else
+			{
+			    System.out.println("sorry songs is not in the list ");
+				
+			}
+			System.out.println("motivational songs ");
+			System.out.println("1.Edhirthu Nill     ");
+			System.out.println("2.Oru nalil   ");
+			System.out.println("3.Nimirnthu NIL     ");
+			System.out.println("4.Ungakkule Mirugam     ");
+		    System.out.println("select your options");
+			//int check=sc.nextInt();
+		    if(check==1)
+			{
+			   
+			   System.out.println("1.Edhirthu Nill   ");
+				
+			}
+			else if(check==2)
+			{
+			   
+			   System.out.println("2.Oru nalil    ");
+				
+			}
+			else if(check==3)
+			{
+			    System.out.println("3.Nimirnthu NIL    ");
+		         
+				
+			}
+			else if(check==4)
+			{
+			    
+			     System.out.println("4.Ungakkule Mirugam   ");
+				 
+			}
+			else
+			{
+			    System.out.println("sorry songs is not in the list ");
+				
+			}
+			System.out.println("sad songs ");
+			System.out.println("1.Kadhal valarthen ");
+			System.out.println("2.Yedho ondru ennai  ");
+			System.out.println("3.Pogadhe   ");
+			System.out.println("4.Ninaithu ninaithu    ");
+		    System.out.println("select your options");
+		    System.out.println("select your options");
+		    //int check=sc.nextInt();
+		    if(check==1)
+			{
+			  System.out.println("1.Kadhal valarthen  ");
+			
+		    }
+			else if(check==2)
+			{
+			   
+			  System.out.println("2.Yedho ondru ennai  ");
+				
+			}
+			else if(check==3)
+			{
+			  System.out.println("3.Pogadhe   ");
+		      
+				
+			}
+			else if(check==4)
+			{
+			    System.out.println("4.Ninaithu ninaithu    ");
+			}	
+			else
+			{
+			    System.out.println("sorry songs is not in the list ");
+				
+			}
+		}	
+		else if(song==5)
+		{
+			System.out.println("ANIRUDH SONGS LISTS ");
+			System.out.println("melody songs ");
+			System.out.println("1.Neeyum Nanum Anbe  ");
+			System.out.println("2.Velicha Poove  ");
+			System.out.println("3.Po Indru Neeyaga   ");
+			System.out.println("4.Nee partha vizhigal    ");
+		    System.out.println("select your options");
+		    int list=sc.nextInt();
+			if(list==1)
+			{
+			   System.out.println("1.Neeyum Nanum Anbe ");
+				
+			}
+			else if(list==2)
+			{
+			   System.out.println("2.Velicha Poove  ");
+				
+			}
+			else if(list==3)
+			{
+			   System.out.println("3.Po Indru Neeyaga  ");
+				
+			}
+			else if(list==4)
+			{
+			    System.out.println("4.Nee partha vizhigal   ");
+				 
+			}
+			else
+			{
+			    System.out.println("sorry songs is not in the list ");
+				
+			}
+			System.out.println("kuthu songs ");
+			System.out.println("1.Vaathi Raid    ");
+			System.out.println("2.Naa Ready    ");
+			System.out.println("3.Badass  ");
+			System.out.println("4.Vandha Edam   ");
+		    System.out.println("select your options");
+		    //int list=sc.nextInt();
+			if(list==1)
+			{
+			   System.out.println("1.Vaathi Raid    ");
+				
+			}
+			else if(list==2)
+			{
+			   System.out.println("2.Naa Ready   ");
+				
+			}
+			else if(list==3)
+			{
+			   System.out.println("3.Badass  ");
+				
+			}
+			else if(list==4)
+			{
+			    System.out.println("4.Vandha Edam  ");
+				 
+			}
+			else
+			{
+			    System.out.println("sorry songs is not in the list ");
+				
+			}
+			System.out.println("motivationalsongs ");
+			System.out.println("1.Velai Illa pattathari     ");
+			System.out.println("2.Boomi Enna Suthudhe     ");
+			System.out.println("3.Ethir Neechal   ");
+			System.out.println("4.Surviva   ");
+		    System.out.println("select your options");
+			//int list=sc.nextInt();
+		    if(list==1)
+			{
+			   System.out.println("1.Velai Illa pattathari     ");
+				
+			}
+			else if(list==2)
+			{
+			   System.out.println("2.Boomi Enna Suthudhe    ");
+				
+			}
+			else if(list==3)
+			{
+			   System.out.println("3.Ethir Neechal   ");
+				
+			}
+			else if(list==4)
+			{
+			    System.out.println("4.Surviva   ");
+				 
+			}
+			else
+			{
+			    System.out.println("sorry songs is not in the list ");
+				
+			}
+			System.out.println("sad songs ");
+			System.out.println("1.Porkanda Singam     ");
+			System.out.println("2.Kanave Kanave      ");
+			System.out.println("3.Kannaana Kanne    ");
+			System.out.println("4.Jodi Nilave   ");
+		    System.out.println("select your options");
+		    //int list=sc.nextInt();
+			if(list==1)
+			{
+			   System.out.println("1.Porkanda Singam      ");
+				
+			}
+			else if(list==2)
+			{
+			   System.out.println("2.Kanave Kanave     ");
+				
+			}
+			else if(list==3)
+			{
+			   System.out.println("3.Kannaana Kanne   ");
+				
+			}
+			else if(list==4)
+			{
+			    System.out.println("4.Jodi Nilave    ");
+				 
+			}
+			else
+			{
+			    System.out.println("sorry songs is not in the list ");
+				
+			}
+		}
+			
+			
+		
+			
+		    	
+			
+			
+		
+			
+					
+			
+			
+			
+			
+			
+		
+		
+	}
+}
